58287b54ff6a3c99d77229e0b85a6774ad61d945
Aditya Nair <adinair.in@gmail.com>
2022-11-21 18:18:42 -0600

    Added license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,51 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//--Returns a license badge based on which license is passed in--
+//--If there is no license, returns an empty string--
+function renderLicenseBadge(license) {
+  if (license !== "no license") {
+    return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
+  } else {
+    return " ";
+  }
+}
+//--Returns the license link--
+//--If there is no license, return an empty string--
+function renderLicenseLink(license) {
+  if (license !== "no license") {
+    return `[${license}](https://choosealicense.com/licenses/${license})`;
+  } else {
+    return " ";
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+//--Returns the license section of README--
+//--If there is no license, return an empty string--
+function renderLicenseSection(license) {
+  if (license !== "no license") {
+    return `
+    ##[License](#table-of-contents)
+    This application has the following license:
+    ${renderLicenseLink(license)}
+    `;
+  } else {
+    return " ";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+//--Generates markdown for README--
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+  * [Description](#desccription)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
 
 `;
 }
